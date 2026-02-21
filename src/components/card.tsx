55d838534d33b6d90d7d3f7762157744c91d90d5
Josh-Wagner1 <josh.will.wagner@gmail.com>
2026-02-21 18:45:55 +1000

    Built Frontpage 1.0

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,14 +1,16 @@
 import "./card.css";
 
 interface CardProps {
+    image: string;
     title: string;
     subtitle?: string;
     content: string
 }
 
-export default function Card({title = "Title", subtitle = "Subtitle", content = "Content"}: CardProps) {
+export default function Card({image, title = "Title", subtitle = "Subtitle", content = "Content"}: CardProps) {
     return (
         <div className="card">
+            <img src={image} alt="Image" className="image"/>
             <h2>{title}</h2>
             <h3>{subtitle}</h3>
             <p>{content}</p>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,10 @@
 import "../App.css"
-import Button from "../components/button"
+/*import Button from "../components/button"*/
 import Card from "../components/card"
 import NavBar from "../components/navbar"
 import Slideshow from "../components/slideshow"
 
 function Home() {
-  const handleClick = () => {
-    alert("Button clicked!");
-  };
 
   const pics = [
     "/images/Apple.jpg",
@@ -22,20 +19,20 @@ function Home() {
       </>
       
       <div>
-        <div className="container">
-          <h1>Welcome to A-Store</h1>
-          <Button className="proceedButton" label="Click Me!" onClick={handleClick}/>
-          <Button className="proceedButton" label="Disabled" onClick={handleClick} disabled={true}/>
-        </div>
-
-        <div className="container">
-          <Card title="Title" subtitle="Subtitle" content="Content"/>
-          <Card title="Title2" subtitle="Subtitle2" content="Content2"/>
-        </div>
-        
+        <h2 className="section">Today's Best Sellers:</h2>
         <div className="container">
           <Slideshow images={pics} delay={5000}/>
         </div>
+
+        <h2 className="section">Popular:</h2>
+        <div className="container">
+          <Card image="/images/Apple.jpg" title="Title" subtitle="Subtitle" content="Content"/>
+          <Card image="/images/Banana.jpg" title="Title2" subtitle="Subtitle2" content="Content2"/>
+          <Card image="/images/Carrot.jpg" title="Title3" subtitle="Subtitle3" content="Content3"/>
+          <Card image="/images/Carrot.jpg" title="Title4" subtitle="Subtitle4" content="Content4"/>
+        </div>
+        
+        
       </div>
     </body>
   )
